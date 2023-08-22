6481713f918f707d6c301c7f1ec57d002db93fa2
Colin McNamara <coolinmc6@gmail.com>
2023-08-22 16:56:51 -0400

    Remove images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
  // Add basePath
   basePath: '/playaround-2023',
+
+  experimental: {
+    images: {
+      enabled: false,
+    }
+  }
 }
 
 module.exports = nextConfig
