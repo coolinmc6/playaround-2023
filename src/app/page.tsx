b4d696f0ee4e4d7a86abf673e7e2822b51bad44a
Colin McNamara <coolinmc6@gmail.com>
2024-03-01 17:06:49 -0500

    Add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import { addToast, type Severity } from '@store/toast';
+import Image from 'next/image';
 import Button from '@/core/Button';
 import Link from '@/core/Link'
 
@@ -22,8 +23,13 @@ const Home = () => {
   return (
     <main className="min-h-screen items-center p-12">
       <h1 className="text-3xl mb-2 text-center">React Playaround 2023</h1>
-      View the app on <Link to="https://playaround-2023.vercel.app/">Vercel</Link>{' '}
-      or the code on <Link to="https://github.com/coolinmc6/playaround-2023">GitHub</Link>.
+      <p className="text-center pb-4">
+        <Image className="inline" src="/assets/logo-cleaned-up.png" alt="repo logo" width="250" height="350"/>
+      </p>
+      <p className="text-center pb-4">    
+        View the app on <Link to="https://playaround-2023.vercel.app/">Vercel</Link>{' '}
+        or the code on <Link to="https://github.com/coolinmc6/playaround-2023">GitHub</Link>.
+      </p>
       <div className="text-center">
         <Button variant="primary" onClick={addRandomToast}>Add Random Toast</Button>
       </div>
